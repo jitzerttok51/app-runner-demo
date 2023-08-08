6f0a8fa285eee6f02227678bef63867157a09c0c
Prodanov, Nikolay <Nikolay.Prodanov@softwareag.com>
2023-08-08 15:08:41 +0300

    Initial commit

---
+++ src/main/java/com/example/apprunnerdemo/AppRunnerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apprunnerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRunnerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRunnerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apprunnerdemo/AppRunnerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apprunnerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRunnerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
